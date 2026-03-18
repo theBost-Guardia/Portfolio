a78286aa5887d50e5e1b9c9baf1ff311ac65769e
Bost <mhenriot@guardiaschool.fr>
2026-03-18 18:57:56 +0100

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,67 @@
+const projectsData = [
+  {
+    "title": "Audit sécurité WiFi",
+    "desc": "Étude des vulnérabilités WPA2 avec Aircrack-ng. Analyse des vecteurs d'attaque.",
+    "img": "assets/aircrack-ng.png"
+  },
+{
+  "title": "Bot Discord",
+  "desc": "Développement d'un bot multifonction sur Discord avec gestion des permissions, logs de modération, commandes personnalisées et autres.",
+  "img": "assets/bot_discord.png"
+},
+{
+  "title": "Portfolio Web",
+  "desc": "Conception d'un portfolio personnel en HTML/CSS avec un focus sur l'OPSEC et le design responsive.",
+  "img": "assets/portfolio_image.png"
+},
+{
+  "title": "Jeu Python - Platformer",
+  "desc": "Développement d'un jeu simple en Python. Apprentissage de la logique de programmation et de la gestion des collisions.",
+  "img": "assets/CelesteClassic.png"
+},
+{
+  "title": "Infrastructure Serveur",
+  "desc": "Configuration et sécurisation d'un serveur de jeux. Gestion des permissions et monitoring, avec parfois quelques mods.",
+  "img": "assets/pterodactyl.png"
+},
+{
+  "title": "Keylogger",
+  "desc": "Analyse du fonctionnement des keyloggers à des fins éducatives. Compréhension des mécanismes de persistance et des méthodes de détection.",
+  "img": "assets/keyloger.png"
+},
+{
+  "title": "Scripts d'automatisation",
+  "desc": "Création de scripts Batch pour automatiser des tâches système. Optimisation des workflows et gain de productivité, ainsi que des scripts Shell ou VBS.",
+  "img": "assets/script_auto.jpg"
+},
+{
+  "title": "↑↑↓↓←→←→BA",
+  "desc": "Ajout d'un Konami Code à mon portfolio.",
+  "img": "assets/Konami-Code.png"
+},
+{
+  "title": "Game of Life Portfolio",
+  "desc": "Ajout d'un easter egg dans le portfolio qui permet de jouer au Jeu de la Vie (Game of Life).",
+  "img": "assets/game_of_life.png"
+},
+{
+  "title": "Packet-Tracer",
+  "desc": "Création de réseaux (LAN avec VLAN + configuration serveur) sur Cisco Packet Tracer.",
+  "img": "assets/packet-tracer.png"
+},
+{
+  "title": "Serveur Proxmox",
+  "desc": "Mise en place d'un serveur privé Proxmox accessible via VPN avec plusieurs services.",
+  "img": "assets/Proxmox.png"
+},
+{
+  "title": "Veille cyber n8n",
+  "desc": "Création d'un serveur n8n sur Proxmox connecté à des flux RSS tels que ceux de l'ANSSI et relié à un webhook Discord pour faire de la veille.",
+  "img": "assets/N8N.png"
+},
+{
+  "title": "Simulation Gestion De Crise",
+  "desc": "Mise en place d'un outil permettant de simuler, à l'aide d'un panel admin et d'un panel de visualisation, une crise de tout type (ransomware, fuite de données, etc.). L'outil comprend le scénario et les animations.",
+  "img": "assets/GDC.png"
+}
+];
